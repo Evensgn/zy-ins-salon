640541556f05ebb56e374c2044146ec98a276815
Evensgn <i@evensgn.com>
2018-01-19 19:52:12 +0800

    Finish first version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import abort, redirect, url_for
 from defines import *
 import time
 import query_web
@@ -14,17 +15,26 @@ def home():
     with open('.//' + save_path + '//' + 'update_time.text', 'r', encoding = 'utf8') as f:
         get_time = float(f.read())
         update_time = time.asctime(time.localtime(get_time))
-    return render_template('index.html', update_time = update_time)
+    return render_template('index.html', wrong_input = False, update_time = update_time)
+
+@app.route('/retry', methods=['GET'])
+def retry():
+    update_time = '宇宙大爆炸后的第 7 天'
+    with open('.//' + save_path + '//' + 'update_time.text', 'r', encoding = 'utf8') as f:
+        get_time = float(f.read())
+        update_time = time.asctime(time.localtime(get_time))
+    return render_template('index.html', wrong_input = True, update_time = update_time)
+
 
 @app.route('/query', methods=['POST'])
-def signin():
+def query():
     stuid = request.form['stuid']
     if not stuid.isdigit():
-        pass
+        return redirect('/retry')
     else:
         report = query_web.query_stuid(stuid)
     return render_template('query.html', report = report)
 
 if __name__ == '__main__':
-    update_records.update()
+    # update_records.update()
     app.run()
--- query_web.py
+++ query_web.py
@@ -19,30 +19,30 @@ def query_stuid(stuid):
 	ret.count = 0
 	ret.pages = []
 	for record in salon_records:
-		page = Page()
-		page.title = record[0]	
-		page.count = 0
-		page.blocks = []
+		page_ = Page()
+		page_.title = record[0]	
+		page_.count = 0
+		page_.blocks = []
 		with open('.//' + save_path + '//' + record[0] + '.text', 'r', encoding = 'utf8') as f:
 			page_text = f.read()
 			blocks = re.findall(block_pattern, page_text)
 			for block in blocks:
-				block = Block()
-				block.title = block[0]
-				block.count = 0
+				block_ = Block()
+				block_.title = block[0]
+				block_.count = 0
 				items = re.findall(stuid_pattern, block[1])
 				if len(items) > 0:
 					for item in items:
 						multinums = re.findall(multinum_pattern, item)
 						if len(multinums) > 0:
-							block.count += int(multinums[0])
+							block_.count += int(multinums[0])
 						else:
-							block.count += 1
-				if block.count > 0:
-					block.multiple = block.count > 1
-					page.blocks.append(block)
-				page.count += block.count
-		if page.count > 0:
-			ret.pages.append(page)
-		ret.count += page.count
+							block_.count += 1
+				if block_.count > 0:
+					block_.multiple = block_.count > 1
+					page_.blocks.append(block_)
+				page_.count += block_.count
+		if page_.count > 0:
+			ret.pages.append(page_)
+		ret.count += page_.count
 	return ret
